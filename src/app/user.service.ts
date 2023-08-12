5f650e6d72b2cd86af94693690431a7e277524d2
Almusty1993 <alanamumusty@gmail.com>
2023-08-12 18:55:22 -0400

    Finish

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -9,15 +9,11 @@ import { Innerjoin } from './innerjoin.model';
 import { Ihistory } from './history.model';
 import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
 
-
-  constructor(private http : HttpClient) { }
-
-
-  
+  constructor(private http: HttpClient) {}
 
   private isLoggedIn: boolean = false;
   private isUser: any = '';
@@ -26,7 +22,7 @@ export class UserService {
     return this.isLoggedIn;
   }
 
-  set loggedIn(value : boolean) {
+  set loggedIn(value: boolean) {
     this.isLoggedIn = value;
   }
 
@@ -34,27 +30,21 @@ export class UserService {
     return this.isUser;
   }
 
-  set user(value : any) {
+  set user(value: any) {
     this.isUser = value;
   }
-
-
-
 
   // resultsearch(title?:any,author? : any,category ? :any){
 
   //   return this.http.get<Ibooks[]>('http://localhost:3000/resultsearch/'+title+"/"+author+"/"+category);}
 
-
-
-
-
-
-  resultsearch(title?: string, author?: string, category?: string): Observable<Ibooks[]> {
-   
+  resultsearch(
+    title?: string,
+    author?: string,
+    category?: string
+  ): Observable<Ibooks[]> {
     let queryParams: any = {};
 
-    
     if (title) {
       queryParams.title = title;
     }
@@ -67,344 +57,236 @@ export class UserService {
       queryParams.category = category;
     }
 
-    
     let httpParams = new HttpParams({ fromObject: queryParams });
 
-    return this.http.get<Ibooks[]>('http://localhost:3000/resultsearch', { params: httpParams });
+    return this.http.get<Ibooks[]>('http://localhost:3000/resultsearch', {
+      params: httpParams,
+    });
   }
 
+  // resultsearch(body : any){
 
-    // resultsearch(body : any){
+  //   const options : any = {
+  //     headers: { 'Content-Type': 'application/json' }
+  //   };
 
-    //   const options : any = {
-    //     headers: { 'Content-Type': 'application/json' }
-    //   };
-  
-    //   console.log(body);
-    //   return this.http.get<Ibooks[]>('http://localhost:3000/resultsearch', body);
-    // }
+  //   console.log(body);
+  //   return this.http.get<Ibooks[]>('http://localhost:3000/resultsearch', body);
+  // }
 
+  getinnerjoin(copy_id: any) {
+    return this.http.get<Innerjoin[]>(
+      'http://localhost:3000/getinnerjoin/' + copy_id
+    );
+  }
 
+  getUsers() {
+    return this.http.get<Iusers[]>('http://localhost:3000/fetchPersons');
+  }
 
+  getbooks() {
+    return this.http.get<Ibooks[]>('http://localhost:3000/fetchallbooks');
+  }
 
-  getinnerjoin(copy_id:any){
+  onGethistory(user_id: any) {
+    return this.http.get<Ihistory[]>(
+      'http://localhost:3000/onGethistory/' + user_id
+    );
+  }
 
-    return this.http.get<Innerjoin[]>('http://localhost:3000/getinnerjoin/'+copy_id);}
+  onGetborrow(user_id: any) {
+    return this.http.get<Ihistory[]>(
+      'http://localhost:3000/onGetborrow/' + user_id
+    );
+  }
 
+  gethighcategory(user_id: any) {
+    return this.http.get<Ihistory[]>(
+      'http://localhost:3000/gethighcategory/' + user_id
+    );
+  }
 
-   
+  getrecommand(category: any) {
+    return this.http.get<Ihistory[]>(
+      'http://localhost:3000/getrecommand/' + category
+    );
+  }
 
+  fetchcopy(book_id: any) {
+    return this.http.get<Icopy[]>(
+      'http://localhost:3000/fetchallcopy/' + book_id
+    );
+  }
 
-  getUsers(){
+  getwishlist(user_id: any) {
+    return this.http.get<Ibooks[]>(
+      'http://localhost:3000/getwishlist/' + user_id
+    );
+  }
 
-    return this.http.get<Iusers[]>('http://localhost:3000/fetchPersons');}
+  getborrowedbooks() {
+    return this.http.get<Iborrow[]>('http://localhost:3000/fetchborrowedbooks');
+  }
 
+  SignUp(body: any) {
+    const options: any = {
+      headers: { 'Content-Type': 'application/json' },
+    };
 
+    console.log(body);
+    return this.http.post('http://localhost:3000/SignUp', body, options);
+  }
 
-    
-  getbooks(){
-
-    return this.http.get<Ibooks[]>('http://localhost:3000/fetchallbooks');}
-
-
-    onGethistory(user_id:any){
-
-      return this.http.get<Ihistory[]>('http://localhost:3000/onGethistory/'+user_id);}
-
-
-
-      onGetborrow(user_id:any){
-
-        return this.http.get<Ihistory[]>('http://localhost:3000/onGetborrow/'+user_id);}
-
-
-
-
-      gethighcategory(user_id:any){
-
-        return this.http.get<Ihistory[]>('http://localhost:3000/gethighcategory/'+user_id);}
-
-
-
-
-        getrecommand(category:any){
-
-          return this.http.get<Ihistory[]>('http://localhost:3000/getrecommand/'+category);}
-
-
-
-
-
-          fetchcopy(book_id:any){
-
-            return this.http.get<Icopy[]>('http://localhost:3000/fetchallcopy/'+book_id);}
-
-
-
-
-            getwishlist(user_id:any){
-
-      return this.http.get<Ibooks[]>('http://localhost:3000/getwishlist/'+user_id);}
-
-
-    
-    getborrowedbooks(){
-
-      return this.http.get<Iborrow[]>('http://localhost:3000/fetchborrowedbooks');}
-
-
-
-
-
-      SignUp(body : any){
-
-        const options : any = {
-          headers: { 'Content-Type': 'application/json' }
-        };
-    
-        console.log(body);
-        return this.http.post('http://localhost:3000/SignUp', body, options);
-      }
-
-
-
-
-
-  LogIn(body:any){
-    const options : any = {
-      headers: { 'Content-Type': 'application/json' }
+  LogIn(body: any) {
+    const options: any = {
+      headers: { 'Content-Type': 'application/json' },
     };
 
     console.log(body);
     return this.http.post('http://localhost:3000/postLogIn', body, options);
   }
 
-
-
-
-
-
-
-
-  updatename(email :any){
-
-    const options : any = {
-      headers : {'Content-Type': 'application/json'}
+  updatename(email: any) {
+    const options: any = {
+      headers: { 'Content-Type': 'application/json' },
     };
 
-    return this.http.put('http://localhost:3000/person/updatePassword/'+email, options );
-   
-
+    return this.http.put(
+      'http://localhost:3000/person/updatePassword/' + email,
+      options
+    );
   }
 
-
-
-
-
-  onstatus(book_id :any){
-console.log(book_id)
-    const options : any = {
-      headers : {'Content-Type': 'application/json'}
+  onstatus(book_id: any) {
+    console.log(book_id);
+    const options: any = {
+      headers: { 'Content-Type': 'application/json' },
     };
 
-    return this.http.put('http://localhost:3000/onstatus/'+book_id, options );
-   
-
+    return this.http.put('http://localhost:3000/onstatus/' + book_id, options);
   }
 
-
-
-  updatecopynumber(book_id :any){
-
-    const options : any = {
-      headers : {'Content-Type': 'application/json'}
+  updatecopynumber(book_id: any) {
+    const options: any = {
+      headers: { 'Content-Type': 'application/json' },
     };
 
-    return this.http.put('http://localhost:3000/updatecopynumber/'+book_id, options );
-   
-
+    return this.http.put(
+      'http://localhost:3000/updatecopynumber/' + book_id,
+      options
+    );
   }
 
-
-
-
-
-  onborrow(body : any){
-
-    const options : any = {
-      headers: { 'Content-Type': 'application/json' }
+  onborrow(body: any) {
+    const options: any = {
+      headers: { 'Content-Type': 'application/json' },
     };
 
     console.log(body);
     return this.http.post('http://localhost:3000/onborrow', body, options);
   }
 
-
-
-  addOverdue(body : any){
-
-    const options : any = {
-      headers: { 'Content-Type': 'application/json' }
+  addOverdue(body: any) {
+    const options: any = {
+      headers: { 'Content-Type': 'application/json' },
     };
 
     console.log(body);
     return this.http.post('http://localhost:3000/addoverdue', body, options);
   }
 
-
-  addhistory(body : any){
-
-    const options : any = {
-      headers: { 'Content-Type': 'application/json' }
+  addhistory(body: any) {
+    const options: any = {
+      headers: { 'Content-Type': 'application/json' },
     };
 
     console.log(body);
     return this.http.post('http://localhost:3000/addhistory', body, options);
   }
 
-
-
-  addwishlist(body : any){
-
-    const options : any = {
-      headers: { 'Content-Type': 'application/json' }
+  addwishlist(body: any) {
+    const options: any = {
+      headers: { 'Content-Type': 'application/json' },
     };
 
     console.log(body);
     return this.http.post('http://localhost:3000/addwishlist', body, options);
-
   }
-  
 
+  getoverduebooks() {
+    return this.http.get<Ioverdue[]>('http://localhost:3000/fetchoverduebooks');
+  }
 
-  getoverduebooks(){
+  updatefine(borrow_id: any, data: any) {
+    const options: any = {
+      headers: { 'Content-Type': 'application/json' },
+    };
 
-    return this.http.get<Ioverdue[]>('http://localhost:3000/fetchoverduebooks');}
+    return this.http.put(
+      'http://localhost:3000/updateoverdue/' + borrow_id,
+      data,
+      options
+    );
+  }
 
+  deletewishlist(book_id: any) {
+    return this.http.delete('http://localhost:3000/deletewishlist/' + book_id);
+  }
 
+  deletePerson(user_id: any) {
+    return this.http.delete('http://localhost:3000/deletePersons/' + user_id);
+  }
 
+  deleteoverdue(borrow_id: any) {
+    return this.http.delete('http://localhost:3000/deleteoverdue/' + borrow_id);
+  }
 
+  deleteborrow(copy_id: any) {
+    return this.http.delete('http://localhost:3000/deleteborrow/' + copy_id);
+  }
 
-    
+  deleteBook(book_id: any) {
+    return this.http.delete('http://localhost:3000/deleteBook/' + book_id);
+  }
 
-    updatefine(borrow_id :any ,data : any){
+  deletewholecopyBook(book_id: any) {
+    return this.http.delete(
+      'http://localhost:3000/deletewholecopioes/' + book_id
+    );
+  }
 
-      const options : any = {
-        headers : {'Content-Type': 'application/json'}
-      };
-  
-      return this.http.put('http://localhost:3000/updateoverdue/'+borrow_id,data, options );
-     
-  
-    }
+  deleteBookcopy(copy_id: any) {
+    return this.http.delete('http://localhost:3000/deleteBookcopy/' + copy_id);
+  }
 
-    deletewishlist(book_id:any){
-      return this.http.delete('http://localhost:3000/deletewishlist/'+book_id);
-    }
-   
-    deletePerson(user_id:any){
-      return this.http.delete('http://localhost:3000/deletePersons/'+user_id);
-    }
+  onAdd(formData: FormData) {
+    return this.http.post('http://localhost:3000/onAdd', formData);
+  }
 
+  updatebook(formData: FormData) {
+    // return this.http.put('http://127.0.0.1:3000/person/updateProducts', formData);
+    return this.http.put('http://localhost:3000/updatebook', formData);
+  }
 
-    deleteoverdue(borrow_id:any){
-      return this.http.delete('http://localhost:3000/deleteoverdue/'+borrow_id);
-    }
+  updateuser(user_id: any, data: any) {
+    const options: any = {
+      headers: { 'Content-Type': 'application/json' },
+    };
+    return this.http.put(
+      'http://localhost:3000/updateuser/' + user_id,
+      data,
+      options
+    );
+  }
 
-
-
-    deleteborrow(copy_id:any){
-      return this.http.delete('http://localhost:3000/deleteborrow/'+copy_id);
-    }
-
-
-
-
-    deleteBook(book_id:any){
-      return this.http.delete('http://localhost:3000/deleteBook/'+book_id);
-    }
-
-
-  
-
-    
-
- 
-
-    deletewholecopyBook(book_id:any){
-    
-      
-      return this.http.delete('http://localhost:3000/deletewholecopioes/'+book_id);
-    }
-   
-
-
-
-
-
-
-    deleteBookcopy(copy_id:any){
-    
-      
-      return this.http.delete('http://localhost:3000/deleteBookcopy/'+copy_id);
-    }
-    
-
-   
-        
-        
-
-
-
-   
-  
-    onAdd(formData:FormData){
-   
-
-     
-   return this.http.post('http://localhost:3000/onAdd', formData);
-    }
-
-
-
-
-
-
-    updatebook(formData:FormData){
-   
-
-      // return this.http.put('http://127.0.0.1:3000/person/updateProducts', formData);
-   return this.http.put('http://localhost:3000/updatebook', formData);
-    }
-
-
-
-
-    updateuser(user_id :any, data : any){
-
-      const options : any = {
-        headers : {'Content-Type': 'application/json'}
-      };
-      return this.http.put('http://localhost:3000/updateuser/'+user_id, data, options );
-   
-
-    }
-  
-
-    updatecopyavailabe(copy_id :any, data : any){
-
-      const options : any = {
-        headers : {'Content-Type': 'application/json'}
-      };
-      return this.http.put('http://localhost:3000/copyavailabe/'+copy_id, data, options );
-   
-
-    }
+  updatecopyavailabe(copy_id: any, data: any) {
+    const options: any = {
+      headers: { 'Content-Type': 'application/json' },
+    };
+    return this.http.put(
+      'http://localhost:3000/copyavailabe/' + copy_id,
+      data,
+      options
+    );
+  }
 }
-
-
-
-
-
-
-
